04a11d998ba1c0b9f59ad0af98eba93a4c60d198
Ian Serlin <ian@ianserlin.com>
2012-10-08 02:09:43 -0700

    minimal amount possible so far, 12k of js

--- resources/javascripts/jet.js
+++ resources/javascripts/jet.js
@@ -1,16 +1,19 @@
-/*
-
-	- process for showing a view
-		0. check if the view is already in the page if it's not an item renderer
-		1a. if not, find and compile the template
-		1b. if it is, grab the current view in the page
-		2. collect all the data associated with the page
-		3. render the page
-		4. transition the page onto the screen
-*/
-
-
 $script.ready('bundle', function() {
+
+;(function($){
+
+    $.getScript = function(url, success, error){
+        var script = document.createElement("script"),
+            $script = $(script);
+        script.src = url;
+
+        $("head").append(script);
+        $script.bind("load", success);
+        $script.bind("error", error);
+    };
+
+})(Zepto);
+
 
 // PROTOTYPE
 
@@ -103,75 +106,6 @@ Jet.prototype.initializeActions = function(){
 	});
 };
 
-// Jet.prototype.watch = function(target, prop, handler){
-// 	if( target.__lookupGetter__(prop) != null ){
-// 		return true;
-// 	}
-// 	var oldval = target[prop], newval = oldval,
-// 	self = this,
-// 	getter = function () {
-// 		return newval;
-// 	},
-// 	setter = function (val) {
-// 		if( self.$.isArray(val) ){
-// 			val = self._extendArray(val, handler, self);
-// 		}
-// 		oldval = newval;
-// 		newval = val;
-// 		handler.call(target, prop, oldval, val, self);
-// 	};
-// 	if (delete target[prop]) { // can't watch constants
-// 		if (Object.defineProperty) { // ECMAScript 5
-// 			Object.defineProperty(target, prop, {
-// 				get: getter,
-// 				set: setter,
-// 				enumerable: false,
-// 				configurable: true
-// 			});
-// 		} else if (Object.prototype.__defineGetter__ && Object.prototype.__defineSetter__) { // legacy
-// 			Object.prototype.__defineGetter__.call(target, prop, getter);
-// 			Object.prototype.__defineSetter__.call(target, prop, setter);
-// 		}
-// 	}
-// 	return this;
-// };
-
-// Jet.prototype.unwatch = function(target, prop){
-// 	var val = target[prop];
-// 	delete target[prop]; // remove accessors
-// 	target[prop] = val;
-// 	return this;
-// };
-
-// Allows operations performed on an array instance to trigger bindings
-// Jet.prototype._extendArray = function(arr, callback, motive){
-// 	if (arr.__wasExtended === true) return;
-
-// 	function generateOverloadedFunction(target, methodName, self){
-// 		return function(){
-// 			var oldValue = Array.prototype.concat.apply(target);
-// 			var newValue = Array.prototype[methodName].apply(target, arguments);
-// 			target.updated(oldValue, motive);
-// 			return newValue;
-// 		};
-// 	} 
-// 	arr.updated = function(oldValue, self){
-// 		callback.call(this, 'items', oldValue, this, motive);
-// 	};
-// 	arr.concat 	= generateOverloadedFunction(arr, 'concat', motive);
-// 	arr.join	= generateOverloadedFunction(arr, 'join', motive);
-// 	arr.pop 	= generateOverloadedFunction(arr, 'pop', motive);
-// 	arr.push 	= generateOverloadedFunction(arr, 'push', motive);
-// 	arr.reverse = generateOverloadedFunction(arr, 'reverse', motive);
-// 	arr.shift 	= generateOverloadedFunction(arr, 'shift', motive);
-// 	arr.slice 	= generateOverloadedFunction(arr, 'slice', motive);
-// 	arr.sort 	= generateOverloadedFunction(arr, 'sort', motive);
-// 	arr.splice 	= generateOverloadedFunction(arr, 'splice', motive);
-// 	arr.unshift = generateOverloadedFunction(arr, 'unshift', motive);
-// 	arr.__wasExtended = true;
-// 	return arr;
-// }
-
 // EVENT HANDLERS
 
 // LOAD API
@@ -224,45 +158,16 @@ Jet.prototype.checkForUpdates = function(){
 Jet.prototype.launch = function(){
 	var self = this;
 	// this.checkForUpdates();
-	async.waterfall([
-		function(callback){
-			self.load(self.config.applicationUrl, function(definition){
-				self.definition = definition;
-				callback(null, definition);
-			});
-		}
-		, function(definition, callback){
-			$('head').append('<link rel="stylesheet" type="text/css" href="'+definition.styles+'"/>');
-			async.auto({
-				templates: function(callback){
-					self.load(definition.templates, function(templates){
-						self.templateDefintion = templates;
-						$(templates).appendTo('body');
-						self.body.find('[type="text/x-handlebars"]').each(function(index, element){
-							var $element = $(element);
-							self.templates[$element.attr('id')] = Handlebars.compile($element.text());
-						});
-						callback(null, templates);
-					});
-				}
-				, data: function(callback){
-					self.load(definition.data, function(data){
-						self.dataDefinition = data;
-						callback(null, data);
-					});
-				}
-				, views: ['templates', 'data', function(callback){
-					self.load(definition.views, function(views){
-						self.viewDefinition = views;
-						self.initializeViews();
-						self.initializeActions();
-						callback(null, views);
-					});
-				}]
-			}, callback);
-		}
-	], function(err, results){
-		console.log('boot complete', err, results);
+	self.load(self.config.applicationUrl, function(definition){
+		self.definition 	= definition;
+		self.viewDefinition = definition.views;
+		self.dataDefinition	= definition.proxies;
+		$('head').append('<link rel="stylesheet" type="text/css" href="'+definition.styles+'"/>');
+		$.getScript(definition.templates, function(){
+			self.templates = Handlebars.templates;
+			self.initializeActions();
+			self.initializeViews();	
+		});
 	});
 	return this;
 };
